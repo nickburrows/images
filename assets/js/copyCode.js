eca1b866cde8bf1b29de76fc95333c4738c940f1
nickburrows <nick.github1@gmail.com>
2022-05-24 18:45:16 +0000

    Deploying to gh-pages from @ nickburrows/images@72c6fe532baf8dcbd83c4fa9f89fa8bf8e1958d7 🚀

--- assets/js/copyCode.js
+++ assets/js/copyCode.js
@@ -26,96 +26,154 @@
 //   })
 // })
 
-const allCodes = document.querySelectorAll(".highlighter-rouge > .highlight > pre > code")
-// const countID = 0
+document.body.onload = addElement
 
-allCodes.forEach((code, i) => {
-  const codeText = allCodes[i].innerText
-  code.setAttribute("id", "code" + i)
-  // const materialIcon = document.createElement("span")
-  //   materialIcon.className = "material-icons"
-  //   materialIcon.innerHTML = "content_paste"
+// function addElement() {
+//   // Material Icons 剪貼簿Icon
+//   const materialIcon = document.createElement("span")
+//     materialIcon.className = "material-icons"
+//     materialIcon.innerHTML = "assignment"
 
-  const iconSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-  const iconPath1 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-  const iconPath2 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+//   // div.tooltips > span.tooltiptext + span.material-icons
+//   const tooltipButton = document.createElement("div")
+//     tooltipButton.className = "tooltip"
+//   const tooltiopText = document.createElement("span")
+//     tooltiopText.className = "tooltiptext"
+//     tooltiopText.innerText = "Copied!"
+//     tooltipButton.appendChild(tooltiopText)
+//     tooltipButton.appendChild(materialIcon)
 
-  iconSvg.setAttribute('height', '16');
-  iconSvg.setAttribute('width', '16');
-  iconSvg.setAttribute('fill', 'currentColor');
-  iconSvg.setAttribute('viewBox', '0 0 16 16');
-  iconSvg.classList.add('bi', 'bi-clipboard2');
+//   var currentDiv = document.getElementById("test-box");
+//   currentDiv.appendChild(tooltipButton)
+// }
 
-  iconPath1.setAttribute(
-    'd',
-    'M3.5 2a.5.5 0 0 0-.5.5v12a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-12a.5.5 0 0 0-.5-.5H12a.5.5 0 0 1 0-1h.5A1.5 1.5 0 0 1 14 2.5v12a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-12A1.5 1.5 0 0 1 3.5 1H4a.5.5 0 0 1 0 1h-.5Z'
-  );
-  iconPath2.setAttribute(
-    'd',
-    'M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5Z'
-  );
-
-
-  iconSvg.appendChild(iconPath1);
-  iconSvg.appendChild(iconPath2);
-
-  const btn = document.createElement("button")
-
-  btn.className = "copy-code-button"
-  btn.setAttribute("data-clipboard-action", "copy")
-  btn.setAttribute("data-clipboard-target", "#code" + i)
-
-  // btn.appendChild(materialIcon)
-  btn.appendChild(iconSvg)
-  let div = document.createElement("div")
-  div.appendChild(btn)
-
-  code.before(div)
-
-  const copiedIconSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-  const copiedIconPath1 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-  const copiedIconPath2 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-  const copiedIconPath3 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-
-  copiedIconSvg.setAttribute('height', '16');
-  copiedIconSvg.setAttribute('width', '16');
-  copiedIconSvg.setAttribute('fill', 'currentColor');
-  copiedIconSvg.setAttribute('viewBox', '0 0 16 16');
-  copiedIconSvg.classList.add('bi', 'bi-clipboard2-check');
-
-  copiedIconPath1.setAttribute(
-    'd',
-    'M9.5 0a.5.5 0 0 1 .5.5.5.5 0 0 0 .5.5.5.5 0 0 1 .5.5V2a.5.5 0 0 1-.5.5h-5A.5.5 0 0 1 5 2v-.5a.5.5 0 0 1 .5-.5.5.5 0 0 0 .5-.5.5.5 0 0 1 .5-.5h3Z'
-  );
-  copiedIconPath2.setAttribute(
-    'd',
-    'M3 2.5a.5.5 0 0 1 .5-.5H4a.5.5 0 0 0 0-1h-.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1H12a.5.5 0 0 0 0 1h.5a.5.5 0 0 1 .5.5v12a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5v-12Z'
-  );
-  copiedIconPath3.setAttribute(
-    'd',
-    'M10.854 7.854a.5.5 0 0 0-.708-.708L7.5 9.793 6.354 8.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3Z'
+function addElement() {
+  const allCodes = document.querySelectorAll(
+    ".highlighter-rouge > .highlight > pre > code"
   )
+  // const countID = 0
 
-  copiedIconSvg.appendChild(copiedIconPath1);
-  copiedIconSvg.appendChild(copiedIconPath2);
-  copiedIconSvg.appendChild(copiedIconPath3);
+  allCodes.forEach((code, i) => {
+    const codeText = allCodes[i].innerText
+    code.setAttribute("id", "code" + i)
+    // const materialIcon = document.createElement("span")
+    //   materialIcon.className = "material-icons"
+    //   materialIcon.innerHTML = "content_paste"
 
-  btn.addEventListener("click", () => {
-    window.navigator.clipboard.writeText(codeText)
+    const iconSvg = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    )
+    const iconPath1 = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "path"
+    )
+    const iconPath2 = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "path"
+    )
 
-    btn.removeChild(iconSvg)
-    btn.appendChild(copiedIconSvg)
+    iconSvg.setAttribute("height", "20")
+    iconSvg.setAttribute("width", "20")
+    iconSvg.setAttribute("fill", "currentColor")
+    iconSvg.setAttribute("viewBox", "0 0 16 16")
+    iconSvg.classList.add("bi", "bi-clipboard2")
 
-    setTimeout(() => {
-      btn.removeChild(copiedIconSvg)
-      btn.appendChild(iconSvg)
-    }, 2000)
+    iconPath1.setAttribute(
+      "d",
+      "M3.5 2a.5.5 0 0 0-.5.5v12a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-12a.5.5 0 0 0-.5-.5H12a.5.5 0 0 1 0-1h.5A1.5 1.5 0 0 1 14 2.5v12a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-12A1.5 1.5 0 0 1 3.5 1H4a.5.5 0 0 1 0 1h-.5Z"
+    )
+    iconPath2.setAttribute(
+      "d",
+      "M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5Z"
+    )
+
+    iconSvg.appendChild(iconPath1)
+    iconSvg.appendChild(iconPath2)
+
+    const materialIcon = document.createElement("span")
+    materialIcon.className = "material-icons"
+    materialIcon.innerHTML = "assignment"
+
+    const btn = document.createElement("button")
+
+    btn.className = "copy-code-button"
+    btn.setAttribute("data-clipboard-action", "copy")
+    btn.setAttribute("data-clipboard-target", "#code" + i)
+    btn.appendChild(materialIcon)
+
+    const tooltipButton = document.createElement("div")
+    tooltipButton.className = "tooltip"
+    const tooltiopText = document.createElement("span")
+    tooltiopText.className = "tooltiptext"
+    tooltiopText.innerText = "Copied!"
+    tooltipButton.appendChild(btn)
+    tooltipButton.appendChild(tooltiopText)
+
+    const copiedIconSvg = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    )
+    const copiedIconPath1 = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "path"
+    )
+    const copiedIconPath2 = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "path"
+    )
+    const copiedIconPath3 = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "path"
+    )
+
+    copiedIconSvg.setAttribute("height", "16")
+    copiedIconSvg.setAttribute("width", "16")
+    copiedIconSvg.setAttribute("fill", "currentColor")
+    copiedIconSvg.setAttribute("viewBox", "0 0 16 16")
+    copiedIconSvg.classList.add("bi", "bi-clipboard2-check")
+
+    copiedIconPath1.setAttribute(
+      "d",
+      "M9.5 0a.5.5 0 0 1 .5.5.5.5 0 0 0 .5.5.5.5 0 0 1 .5.5V2a.5.5 0 0 1-.5.5h-5A.5.5 0 0 1 5 2v-.5a.5.5 0 0 1 .5-.5.5.5 0 0 0 .5-.5.5.5 0 0 1 .5-.5h3Z"
+    )
+    copiedIconPath2.setAttribute(
+      "d",
+      "M3 2.5a.5.5 0 0 1 .5-.5H4a.5.5 0 0 0 0-1h-.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1H12a.5.5 0 0 0 0 1h.5a.5.5 0 0 1 .5.5v12a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5v-12Z"
+    )
+    copiedIconPath3.setAttribute(
+      "d",
+      "M10.854 7.854a.5.5 0 0 0-.708-.708L7.5 9.793 6.354 8.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3Z"
+    )
+
+    copiedIconSvg.appendChild(copiedIconPath1)
+    copiedIconSvg.appendChild(copiedIconPath2)
+    copiedIconSvg.appendChild(copiedIconPath3)
+
+    btn.addEventListener("click", () => {
+      window.navigator.clipboard.writeText(codeText)
+
+      // btn.removeChild(iconSvg)
+      // btn.appendChild(copiedIconSvg)
+
+      tooltiopText.classList.add("visible")
+
+      setTimeout(() => {
+        // btn.removeChild(copiedIconSvg)
+        // btn.appendChild(iconSvg)
+        tooltiopText.classList.remove("visible")
+      }, 2000)
+    })
+
+    let div = document.createElement("div")
+    div.className = "copy-btn"
+    div.appendChild(tooltipButton)
+
+    code.before(div)
   })
 
-})
-
-const clipboard = new ClipboardJS(".copy-code-button")
-
+  const clipboard = new ClipboardJS(".copy-code-button")
+}
 // var allCodeBlocksElements = $("code")
 // const allCodeBlocksElements = document.querySelectorAll(
 //   ".highlight > .highlight"
